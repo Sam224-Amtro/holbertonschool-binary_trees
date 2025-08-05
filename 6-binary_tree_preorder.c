c11016f2945cab12db574f8ec6f9c1bd24da326b
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-08-05 23:25:07 +0200

    It's binary tree using pre-order traversal

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * print_num - Prints a number
+ *
+ * @n: Number to be printed
+ */
+ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+ {
+    if (!tree || !func)
+        return;
+
+    func(tree->n);
+    binary_tree_preorder(tree->left, func);
+    binary_tree_preorder(tree->right, func);
+}
