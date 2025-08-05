4b888722ea2095e81f386ac028126e484e0db32a
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-08-05 23:56:00 +0200

    It is binary tree using traversal

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -3,20 +3,22 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - Goes through a binary tree using pre-order traversal
+ * binary_tree_inorder - Goes through a binary tree using pre-order traversal
  * @tree: Pointer to the root node of the tree to traverse
  * @func: Pointer to a function to call for each node's value
  *
  * Description: This function applies the function 'func' to the value of each
- *              node following the pre-order traversal (Node -> Left -> Right).
+ *              node following the inorder traversal (Left -> Node -> Right).
  *              If 'tree' or 'func' is NULL, the function does nothing.
  */
- void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
  {
     if (!tree || !func)
         return;
 
+    
+    binary_tree_inorder(tree->left, func);
     func(tree->n);
-    binary_tree_preorder(tree->left, func);
-    binary_tree_preorder(tree->right, func);
+    binary_tree_inorder(tree->right, func);
 }
+
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -3,20 +3,21 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - Goes through a binary tree using pre-order traversal
+ * binary_tree_postorder - Goes through a binary tree using pre-order traversal
  * @tree: Pointer to the root node of the tree to traverse
  * @func: Pointer to a function to call for each node's value
  *
  * Description: This function applies the function 'func' to the value of each
- *              node following the pre-order traversal (Node -> Left -> Right).
+ *              node following the postorder traversal (Left -> Righ -> tNode).
  *              If 'tree' or 'func' is NULL, the function does nothing.
  */
- void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
  {
     if (!tree || !func)
         return;
 
+    
+    binary_tree_postorder(tree->left, func);
+    binary_tree_postorder(tree->right, func);
     func(tree->n);
-    binary_tree_preorder(tree->left, func);
-    binary_tree_preorder(tree->right, func);
 }
