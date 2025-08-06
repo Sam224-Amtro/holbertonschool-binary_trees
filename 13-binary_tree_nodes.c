a16d688898c183a770c208332a226559b1f28523
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-08-06 13:46:37 +0200

    Objectif est de compter le nombre de nœuds dans un arbre binaire qui ont au moins un enfant (gauche, droite ou les deux).

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - Counts the nodes with at least one child in a binary tree
+ * @tree: Pointer to the root node of the tree to count the number of nodes
+ *
+ * Return: Number of nodes with at least one child, or 0 if tree is NULL
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+   size_t left_node, right_node;
+
+    /* Si l’arbre est vide (NULL), il ne contient aucun nœud, donc on retourne 0. */
+    if (tree == NULL)
+        return (0);
+
+    /* Si le node n'a pas d'enfant à gauche ni à droite, alors c'est une feuille */
+    if (tree->left == NULL && tree->right == NULL)
+        return (0);
+
+    /* Récursion à gauche et à droite */
+    left_node = binary_tree_nodes(tree->left);   
+    right_node = binary_tree_nodes(tree->right);
+
+        return ((left_node + right_node) + 1);
+}
