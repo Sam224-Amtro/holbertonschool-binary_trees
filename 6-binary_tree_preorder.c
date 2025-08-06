e923b5fc0c9711d3802732f85bc693f375bb43b4
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-08-06 19:09:33 +0200

    les tabulation n'était pas prise en contre avec VS code, j'ai mis des tabulation dans tout les dossiers

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -3,20 +3,20 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - Goes through a binary tree using pre-order traversal
- * @tree: Pointer to the root node of the tree to traverse
- * @func: Pointer to a function to call for each node's value
- *
- * Description: This function applies the function 'func' to the value of each
- *              node following the pre-order traversal (Node -> Left -> Right).
- *              If 'tree' or 'func' is NULL, the function does nothing.
- */
- void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
- {
-    if (!tree || !func)
-        return;
+* binary_tree_preorder - Goes through a binary tree using pre-order traversal
+* @tree: Pointer to the root node of the tree to traverse
+* @func: Pointer to a function to call for each node's value
+*
+* Description: This function applies the function 'func' to the value of each
+*              node following the pre-order traversal (Node -> Left -> Right).
+*              If 'tree' or 'func' is NULL, the function does nothing.
+*/
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
 
-    func(tree->n);
-    binary_tree_preorder(tree->left, func);
-    binary_tree_preorder(tree->right, func);
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -3,22 +3,20 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - Goes through a binary tree using pre-order traversal
- * @tree: Pointer to the root node of the tree to traverse
- * @func: Pointer to a function to call for each node's value
- *
- * Description: This function applies the function 'func' to the value of each
- *              node following the inorder traversal (Left -> Node -> Right).
- *              If 'tree' or 'func' is NULL, the function does nothing.
- */
- void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
- {
-    if (!tree || !func)
-        return;
+* binary_tree_inorder - Goes through a binary tree using pre-order traversal
+* @tree: Pointer to the root node of the tree to traverse
+* @func: Pointer to a function to call for each node's value
+*
+* Description: This function applies the function 'func' to the value of each
+*              node following the inorder traversal (Left -> Node -> Right).
+*              If 'tree' or 'func' is NULL, the function does nothing.
+*/
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
 
-    
-    binary_tree_inorder(tree->left, func);
-    func(tree->n);
-    binary_tree_inorder(tree->right, func);
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
-
