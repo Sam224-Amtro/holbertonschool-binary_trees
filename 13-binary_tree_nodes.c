e399e90cb790737ce169fb9843880b9ff6a4b858
Samamtro <fofanasanoussy545@gmail.com>
2025-08-06 16:01:42 +0200

    the file to binary_tree_nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -16,7 +16,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	if (tree->left == NULL || tree->right == NULL)
+	if (tree->left == NULL && tree->right == NULL)
 	{
 		return (0);
 	}
